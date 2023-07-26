1d6fc7d28da7384935f889a38ddc0dfe1c085712
Kamil Pigulak <kamil.pigulak@mediaflex.pl>
2023-07-26 08:03:35 +0200

    add data to game

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,24 +1,104 @@
-const dataToGame = [{
+const dataToGame = [
+  {
     id: 1,
     hint1: 'Jest piłkarzem FC Barcelony',
     hint2: 'Gra na pozycji napastnika',
     hint3: 'Jest kapitanem reprezentacji Polski',
     name: 'Robert Lewandowski',
     club: 'FC Barcelona',
-    position: 'Napastnik', 
-    age: '34 lata'
-},
-{
+    position: 'Napastnik',
+    age: '34'
+  },
+  {
     id: 2,
     hint1: 'Jest piłkarzem Napoli',
     hint2: 'Gra na pozycji pomocnika',
     hint3: 'Swoją karierę rozpoczął w Empoli',
     name: 'Piotr Zieliński',
     club: 'Napoli',
-    position: 'Pomocnik', 
-    age: '34 lata'
-}];
+    position: 'Pomocnik',
+    age: '29'
+  },
+  {
+    id: 3,
+    hint1: 'Jest piłkarzem Juventusu Turyn',
+    hint2: 'Gra na pozycji bramkarza',
+    hint3: 'Syn byłego, świetnego bramkarza Lagii Warszawa',
+    name: 'Wojciech Szczęsny',
+    club: 'Juventus Turyn',
+    position: 'Bramkarz',
+    age: '33'
+  },
+  {
+    id: 4,
+    hint1: 'Jest piłkarzem Juventusu Turyn',
+    hint2: 'Gra na pozycji napastnika',
+    hint3: 'Rozpoczął swoją karierę w Górniku Zabrze',
+    name: 'Arkadiusz Milik',
+    club: 'Juventus Turyn',
+    position: 'Napastnik',
+    age: '29'
+  },
+  {
+    id: 5,
+    hint1: 'Jest piłkarzem Juventusu Turyn',
+    hint2: 'Gra na pozycji napastnika',
+    hint3: 'Rozpoczął swoją karierę w Górniku Zabrze',
+    name: 'Arkadiusz Milik',
+    club: 'Juventus Turyn',
+    position: 'Napastnik',
+    age: '29'
+  },
+  {
+    id: 6,
+    hint1: 'Jest piłkarzem Spezii Calcio',
+    hint2: 'Gra na pozycji bramkarza',
+    hint3: 'W bardzo młodym wieku zadebiutował w Jagielonii Białystok',
+    name: 'Bartłomiej Drągowski',
+    club: 'Spezia Calcio',
+    position: 'Bramkarz',
+    age: '25'
+  },
+  {
+    id: 7,
+    hint1: 'Jest piłkarzem Bolognii FC',
+    hint2: 'Gra na pozycji bramkarza',
+    hint3: 'W wywiadzie ujawnił dokładnie jak to było z premiami na Mistrzostawach Świata',
+    name: 'Łukasz Skorupski',
+    club: 'Bologna FC',
+    position: 'Bramkarz',
+    age: '32'
+  },
+  {
+    id: 8,
+    hint1: 'Jest piłkarzem Southampton F.C.',
+    hint2: 'Gra na pozycji obrońcy',
+    hint3: 'Strzelił gola w meczu z Japonią na Mistrzostwach Świata w 2018 roku',
+    name: 'Jan Bednarek',
+    club: 'Southampton F.C.',
+    position: 'Obrońca',
+    age: '27'
+  },
+  {
+    id: 9,
+    hint1: 'Jest piłkarzem Sampdorii',
+    hint2: 'Gra na pozycji obrońcy',
+    hint3: 'W reprezentacji gra najczęściej na lewej obronie mimo, że jest prawym obrońcą',
+    name: 'Bartosz Bereszyński',
+    club: 'UC Sampdoria',
+    position: 'Obrońca',
+    age: '31'
+  },
+  {
+    id: 10,
+    hint1: 'Jest piłkarzem PAOKu Saloniki',
+    hint2: 'Gra na pozycji obrońcy',
+    hint3: 'Gdy rozpoczęła się wojna w Ukrainie, grał w jednym z tamtejszych klubów',
+    name: 'Tomasz Kędziora',
+    club: 'PAOK Saloniki',
+    position: 'Obrońca',
+    age: '29'
+  }
+]
 
-export {
-    dataToGame
-};
+export { dataToGame }
